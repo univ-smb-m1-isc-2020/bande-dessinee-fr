01064b7a2f39b6896d0cabf3fc813b05cea67a3f
Visar <Visar@DESKTOP-8A09G93>
2021-04-22 04:50:33 +0200

    feature

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -4,6 +4,21 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styles: [
+    `
+    div#gauche-col{
+      min-height: 100vh;
+      overflow: auto;
+      background-image:url("https://images.pexels.com/photos/3744196/pexels-photo-3744196.jpeg?auto=compress&cs=tinysrgb&h=650&w=940");
+      background-size: cover;
+    }
+
+    div#droit-col{
+      min-height: 100vh;
+      overflow: auto;
+      background-image:url("https://images.pexels.com/photos/3744196/pexels-photo-3744196.jpeg?auto=compress&cs=tinysrgb&h=650&w=940");
+      background-size: cover;
+    }
+    `
   ]
 })
 export class HomepageComponent implements OnInit {
