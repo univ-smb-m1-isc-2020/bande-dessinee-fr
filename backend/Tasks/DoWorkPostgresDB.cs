296ae9d7402c3d470bc8baf256ea202a5a9b1df9
Visar <Visar@DESKTOP-8A09G93>
2021-04-28 15:38:56 +0200

    en pingant

--- backend/Tasks/DoWorkPostgresDB.cs
+++ backend/Tasks/DoWorkPostgresDB.cs
@@ -3,6 +3,8 @@ using DbUp.ScriptProviders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net.NetworkInformation;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace api.Tasks
@@ -16,7 +18,26 @@ namespace api.Tasks
         public async Task DoWork()
         {
             await Task.Delay(30 * 1000);
-            EnsureDatabase.For.PostgresqlDatabase("Server=postgres;Port=5432;Database=db_bande_dessinee_fr;User Id=bande_dessinee_fr_root;Password=toor;");
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions
+            {
+                DontFragment = true
+            };
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send("postgres", timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+            EnsureDatabase.For.PostgresqlDatabase("Server="+ reply.Address.ToString()+";Port=5432;Database=db_bande_dessinee_fr;User Id=bande_dessinee_fr_root;Password=toor;");
 
             var upgrader = DeployChanges
                 .To
